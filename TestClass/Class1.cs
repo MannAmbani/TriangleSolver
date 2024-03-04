1afcf8eeae7c9365963e8f3981de823e4eaa1ebc
Mann <mann.ambani007@gmail.com>
2024-03-03 23:25:51 -0500

    Add project files.

---
+++ TestClass/Class1.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+
+namespace TestClass
+{
+    public class TriangleTest
+
+    {
+        [TestFixture]
+        public class TriangleTest1
+        {
+            //Equilaterla Triangle Test 1
+            [Test]
+            public void AnalyzeTriangle_Input60and60and60_OutputEquilateralTriangle()
+            {
+                //Arrange
+                int firstSide = 60;
+                int secondSide = 60;
+                int thirdSide = 60;
+                string expected = "Equilateral triangle";
+
+                //Act
+                string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+                //Assert
+                Assert.That(actual, Is.EqualTo(expected));
+            }
+
+        
+
+
+
+
+        }
+    }
+}
